ff8df6faebbbc128235a558c80d6caf581fc1643
binaryatwork <binaryatwork@users.noreply.github.com>
2015-03-29 01:21:57 -0700

    added more comment

--- src/main.c
+++ src/main.c
@@ -2,15 +2,11 @@
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
-int current_hour;
+int current_hour=-1;
 
 
-static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-  
-  current_hour = tick_time->tm_hour;
+static void update_time(struct tm *tick_time) {
+   current_hour = tick_time->tm_hour;
 
   // Create a long-lived buffer
   static char buffer[] = "00:00";
@@ -29,54 +25,57 @@ static void update_time() {
 }
 
 static void tick_handler_hour(struct tm *tick_time) {
-    //int theNum = tick_time->tm_hour;
-    int theNum = tick_time->tm_min;
+    
+    int theNum = tick_time->tm_hour;
+    //int theNum = tick_time->tm_min;
     if ( theNum != current_hour){
-      vibes_short_pulse();
-    }
+      // only change color if the current hour is different from the save hour
+      vibes_short_pulse();    
 
 #ifdef PBL_COLOR   
-    int i = theNum % 12;
-    if (i == 0){
-        window_set_background_color(s_main_window,  GColorWhite );  
-    }else if ( i == 1){
-        window_set_background_color(s_main_window,  GColorCyan);  
-    }else if ( i  == 2){
-        window_set_background_color(s_main_window, GColorIcterine  );  
-    }else if ( i == 3){
-        window_set_background_color(s_main_window, GColorRajah  );  
-    }else if ( i == 4){
-        window_set_background_color(s_main_window, GColorLightGray  );  
-    }else if ( i == 5){
-        window_set_background_color(s_main_window, GColorMintGreen  );  
-    }else if ( i == 6){
-        window_set_background_color(s_main_window, GColorElectricBlue  );  
-    }else if ( i == 7){
-        window_set_background_color(s_main_window, GColorLiberty   );  
-    }else if ( i == 8){
-        window_set_background_color(s_main_window, GColorVividCerulean   );  
-    }else if ( i == 9){
-        window_set_background_color(s_main_window, GColorMelon   );  
-    }else if ( i == 10){
-       window_set_background_color(s_main_window, GColorClear  );  
-    }else if ( i == 11){
-       window_set_background_color(s_main_window, GColorBlueMoon   );  
-    }      
+      // use color background if PBL_COLOR is defined.
+      int i = theNum % 12;
+      if (i == 0){
+          window_set_background_color(s_main_window,  GColorWhite );  
+      }else if ( i == 1){
+          window_set_background_color(s_main_window,  GColorCyan);  
+      }else if ( i  == 2){
+          window_set_background_color(s_main_window, GColorIcterine  );  
+      }else if ( i == 3){
+          window_set_background_color(s_main_window, GColorRajah  );  
+      }else if ( i == 4){
+          window_set_background_color(s_main_window, GColorLightGray  );  
+      }else if ( i == 5){
+          window_set_background_color(s_main_window, GColorMintGreen  );  
+      }else if ( i == 6){
+          window_set_background_color(s_main_window, GColorElectricBlue  );  
+      }else if ( i == 7){
+          window_set_background_color(s_main_window, GColorLiberty   );  
+      }else if ( i == 8){
+          window_set_background_color(s_main_window, GColorVividCerulean   );  
+      }else if ( i == 9){
+          window_set_background_color(s_main_window, GColorMelon   );  
+      }else if ( i == 10){
+         window_set_background_color(s_main_window, GColorClear  );  
+      }else if ( i == 11){
+         window_set_background_color(s_main_window, GColorBlueMoon   );  
+      }      
 #else
-    int i = theNum % 2;
-    if (i == 0){
-        window_set_background_color(s_main_window, GColorWhite);
-        text_layer_set_text_color(s_time_layer, GColorBlack);        
-    }else {      
-        window_set_background_color(s_main_window, GColorBlack );
-        text_layer_set_text_color(s_time_layer, GColorWhite);      
-    }  
+      int i = theNum % 2;
+      if (i == 0){
+          window_set_background_color(s_main_window, GColorWhite);
+          text_layer_set_text_color(s_time_layer, GColorBlack);        
+      }else {      
+          window_set_background_color(s_main_window, GColorBlack );
+          text_layer_set_text_color(s_time_layer, GColorWhite);      
+      }  
 #endif
+    }
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   tick_handler_hour(tick_time);
-  update_time();
+  update_time(tick_time);
 }
 
 static void main_window_load(Window *window) {
@@ -112,8 +111,9 @@ static void init() {
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   
-  // Make sure the time is displayed from the start
-  update_time();
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+  tick_handler(tick_time, MINUTE_UNIT);  
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
